b17c569fd6f1b54195a9392d02a5c3611e47073a
Sampsa Kautto <K2427@LABRANET.JAMK.FI>
2017-09-04 11:03:26 +0300

    Initial commit

---
+++ app/src/main/java/fi/jamk/basicuicontrols2/MainActivity.java
@@ -0,0 +1,13 @@
+package fi.jamk.basicuicontrols2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
